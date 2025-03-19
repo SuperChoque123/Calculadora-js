0725d7a0adc6f7cba100075b0a9e2eb419d2a622
Enzo Ramos <enzo_r_siqueira@estudante.sesisenai.org.br>
2025-03-19 09:59:38 -0300

    Calculadora

---
+++ script.js
@@ -0,0 +1,13 @@
+function calcular(n1,n2) {
+    const soma = n1+n2;
+    const subtracao = n1-n2;
+    const divisao = n2 !== 0 ? n1/n2: "Não pode dividir por zero";
+    const multiplicacao = n1*n2;
+    console.log(soma);
+    console.log(subtracao);
+    console.log(divisao);
+    console.log(multiplicacao);
+}
+let n1 = parseFloat(prompt("Digite seu primeiro número"));
+let n2 = parseFloat(prompt("Digite seu segundo número"));
+    calcular(n1,n2);
